425147d381b66f4bd120743f93ff5909f73c0929
mu-suke <chelsea_08ym@yahoo.co.jp>
2021-05-14 14:35:15 +0900

    Initial Commit

---
+++ Shared/XCUITesting_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCUITesting_PlaygroundApp.swift
+//  Shared
+//
+//  Created by 村上 祐亮 on 2021/05/14.
+//
+
+import SwiftUI
+
+@main
+struct XCUITesting_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
